e9bbb4ad42bec4f8494543893fcd207cc62a25a1
Gustavo Mojica <gmojica@unal.edu.co>
2021-04-06 21:47:17 -0500

    Inicialization

---
+++ backend/prolaw/src/test/java/com/prolaw/prolaw/ProlawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolaw.prolaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProlawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
