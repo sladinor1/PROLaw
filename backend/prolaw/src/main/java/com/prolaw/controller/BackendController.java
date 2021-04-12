bd1d52d1169d3d710ac92aadef12b28e5f82ff12
Gustavo Mojica <gmojica@unal.edu.co>
2021-04-11 22:10:26 -0500

    Spring 1

--- backend/prolaw/src/main/java/com/prolaw/controller/BackendController.java
+++ backend/prolaw/src/main/java/com/prolaw/controller/BackendController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,6 +49,7 @@ public class BackendController {
     private String emailUser;
     private String passUser;
     */
+	//@CrossOrigin(origins = "http://localhost:8080")
 	@ResponseBody
 	@RequestMapping(path = "/user/{idUser}/{nameUser}/{lastNameUser}/{celUser}/{emailUser}/{passUser}", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
