286750f0232eca0956c97b3eb2670b2459ce6c6a
Harol Achagua <hyachaguac@unal.edu.co>
2021-04-22 13:29:13 -0500

    Mejoras Interfaz Login y Register

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,53 +1,35 @@
 <template>
-  <div class="form-container">
-    <h2>Crear cuenta</h2>
+<div class="form-container">
+    <h2>Inicio de Sesión</h2>
 
     <div id='form' class="p-fluid p-formgrid p-grid">
 
-      <div class="p-field p-col-12 p-md-6">
-        <InputText id="email" type="email" v-model="usuario.email" placeholder="Correo Electrónico"/>
-        <label class="text-danger" v-if="usuario.email">{{validarEmail()}}</label>
-      </div>
-      <br>
-      <div>
-        <Password v-model="usuario.pass" placeholder="Contraseña" toggleMask></Password>
-        <label class="text-danger" v-if="usuario.pass">{{validarContraseña()}}</label>
-      </div>
-      <br>
-      <div>
-        <Dropdown v-model="selectedid" :options="idtypes" optionLabel="name" placeholder="Tipo de documento"></Dropdown>
-      </div>
-      <br>
-      <div class="p-field p-col-12 p-md-6">
-        <InputText id="id" type="text" v-model="usuario.id" placeholder="Número de documento"/>
-        <label class="text-danger" v-if="usuario.id">{{validarId()}}</label>
-      </div>
-      <br>
-      <div class="p-field p-col-12 p-md-6">
-        <InputText id="name" type="text" v-model="usuario.names" placeholder="Nombre(s)"/>
-        <label class="text-danger" v-if="usuario.names">{{validarNombres()}}</label>
-      </div>
-      <br>
-      <div class="p-field p-col-12 p-md-6">
-        <InputText id="ln" type="text" v-model="usuario.lastnames" placeholder="Apellidos"/>
-        <label class="text-danger" v-if="usuario.lastnames">{{validarApellidos()}}</label>
-      </div>
-      <br>
-      <div class="p-field p-col-12 p-md-6">
-        <InputText id="phone" type="text" v-model="usuario.phone" placeholder="Número de teléfono"/>
-        <label class="text-danger" v-if="usuario.phone">{{validarTelefono()}}</label>
-      </div>
-      <br>
-      <div>
-        <Dropdown v-model="selected" :options="cities" optionLabel="label" optionGroupLabel="label" 
-        optionGroupChildren="items" placeholder="Departamento y Ciudad"></Dropdown>
-      </div>  
-      <br>
-      <div class="">
-        <button class="botonRegistro" v-on:click="registrar">Registrarse</button>
-      </div>
+        <div class="p-field p-col-12 p-md-6">
+            <InputText id="email" type="email" v-model="usuario.email" placeholder="Correo Electrónico"/>
+            <label class="text-danger" v-if="usuario.email">{{validarEmail()}}</label>
+        </div>
+        <br>
+        <div>
+            <Password v-model="usuario.pass" placeholder="Contraseña" toggleMask></Password>
+            <label class="text-danger" v-if="usuario.pass">{{validarContraseña()}}</label>
+        </div>
+
+            <p><router-link to="/Register" tag="button" class="btn btn-primary" type="submit">¿Olvidaste tu contraseña?</router-link></p>
+
+        <div class="d-grid gap-2">
+            <button class="botonInicioSesion" v-on:click="getLogin">Iniciar Sesión</button>
+        </div>
+        <div v-if="inside">
+            <label>Bienvenido {{usuario.email}}</label><br><br>
+            <router-link to="">
+                <span v-on:click="logOut">Cerrar sesion</span>
+            </router-link>
+        </div>
+      
     </div>
   </div>
+
+
 </template>
 
 <script>
