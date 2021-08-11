d114684abc530ed06eaafde5feb64d788152e891
Gustavo Mojica <gmojica@unal.edu.co>
2021-08-10 19:33:10 -0500

    1ra Prueba

---
+++ backend/prolaw/src/main/java/com/prolaw/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.prolaw.repository;
+
+import com.prolaw.domain.*;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+public interface NotificationRepository extends CrudRepository<Notification,Long>{
+    Notification findByIdNots(@Param("id_not") long idNots);
+}
+
+
