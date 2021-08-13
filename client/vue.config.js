2a38d1ed0d22968840392654d477ae41b3e99119
Gustavo Mojica <gmojica@unal.edu.co>
2021-08-12 22:37:50 -0500

    external server

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,9 +2,12 @@ module.exports ={
     devServer: {
         proxy: {
             '/api':{
-                target: 'http://localhost:8080',
+                target: 'http://165.227.206.19:8080',
                 ws: true,
-                changeOrigin: true
+                changeOrigin: true,
+                onProxyReq: function(request){
+                    request.setHeader("origin",'http://165.227.206.19:8080');
+                }
             }
         }
     },
